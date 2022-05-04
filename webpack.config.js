f5a8ba0d2ec9211fe59565fbcc5f111fe71d46e9
Julia Khvatova <jkhvatova@gmail.com>
2022-05-04 16:13:17 +0300

    init: start virtual-keyboard task

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/js/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist/virtual-keyboard'),
+    filename: 'index.js',
+    publicPath: '',
+  },
+  devServer: {
+    static: './src',
+    open: true,
+    liveReload: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+
+      {
+        test: /\.js$/,
+        exclude: '/node_modules/',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  devtool: 'source-map',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new ESLintPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: true,
+      chunks: ['index'],
+      filename: 'index.html',
+    }),
+  ],
+};
