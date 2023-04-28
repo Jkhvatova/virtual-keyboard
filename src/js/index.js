32a876464e9426ef1ddd4a0ebb8834a69897a556
Julia Khvatova <jkhvatova@gmail.com>
2023-04-28 22:59:40 +0300

    feat: add all main style and keyboard layouts object

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,7 @@
 import '../sass/main.scss';
+import keyLayouts from './layouts';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // eslint-disable-next-line no-console
+  console.log(keyLayouts);
+});
