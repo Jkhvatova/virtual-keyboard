26ddcecfd1347763d1de330c607fb8e8115f5b4a
Julia Khvatova <jkhvatova@gmail.com>
2023-04-29 23:27:14 +0300

    feat: add more styles to app

--- src/js/index.js
+++ src/js/index.js
@@ -4,8 +4,6 @@ import keyLayouts from './layouts';
 document.addEventListener('DOMContentLoaded', () => {
   const body = document.querySelector('body');
 
-  // eslint-disable-next-line no-console
-  console.log(keyLayouts);
   function createDomElements() {
     const container = document.createElement('div');
     container.className = 'container';
@@ -15,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
     titleAndExplain.className = 'title-and-explanation';
     titleAndExplain.innerHTML = `
       <h1 class="title">Virtual Keyboard</h1>
-      <p class="explanation">Created on MacOS.
+      <p class="explanation">Created on MacOS.<br/>
       To change the language press left ctrl + alt</p>
     `;
     // monitor and text-area
@@ -32,11 +30,63 @@ document.addEventListener('DOMContentLoaded', () => {
     const keyboardWrapper = document.createElement('div');
     keyboardWrapper.className = 'keyboard-wrapper';
     const keyboard = document.createElement('div');
-    keyboardWrapper.className = 'keyboard';
+    keyboard.className = 'keyboard';
     keyboardWrapper.appendChild(keyboard);
     // const keys = document.querySelectorAll('.key');
 
     container.append(titleAndExplain, monitorWrapper, keyboardWrapper);
   }
   createDomElements();
+  function renderKeyboard(keys) {
+    const keyboard = document.querySelector('.keyboard');
+    // eslint-disable-next-line no-restricted-syntax
+    Object.entries(keys).forEach(([key, value]) => {
+      const renderKey = document.createElement('div');
+      renderKey.className = 'key';
+      renderKey.dataset.keycode = key;
+      renderKey.innerHTML = value.en;
+      switch (key) {
+        case 'Backspace':
+          renderKey.classList.add('backspace');
+          break;
+        case 'Tab':
+          renderKey.classList.add('tab');
+          break;
+        case 'Del':
+          renderKey.classList.add('del');
+          break;
+        case 'CapsLock':
+          renderKey.classList.add('caps');
+          break;
+        case 'Enter':
+          renderKey.classList.add('enter');
+          break;
+        case 'ShiftRight':
+        case 'ShiftLeft':
+          renderKey.classList.add('shift');
+          break;
+        case 'ControlLeft':
+        case 'ControlRight':
+          renderKey.classList.add('ctrl');
+          break;
+        case 'AltRight':
+        case 'AltLeft':
+          renderKey.classList.add('alt');
+          break;
+        case 'MetaRight':
+        case 'MetaLeft':
+          renderKey.classList.add('command');
+          break;
+        case 'Space':
+          renderKey.classList.add('space');
+          break;
+        default:
+          break;
+      }
+      keyboard.appendChild(renderKey);
+      // eslint-disable-next-line no-console
+      // console.log(key, renderKey);
+    });
+  }
+  renderKeyboard(keyLayouts);
 });
